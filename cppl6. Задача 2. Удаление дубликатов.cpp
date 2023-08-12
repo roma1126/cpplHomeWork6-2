4ff0e4269da7a6648355894d511aa1ca3acf88dc
Roman <romka200211@gmail.com>
2023-08-12 11:13:09 +0300

    first commit

---
+++ cppl6. Задача 2. Удаление дубликатов.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <set>
+
+template<class T>
+void print(const T& p0)
+{
+    std::cout << "[OUT]:" << std::endl;
+    for (const auto& i : p0)
+    {
+        std::cout << i << std::endl;
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int numberOfElements = 0;
+    int elements = 0;
+    std::set<int, std::greater<>> p1;
+
+    std::cout << "Введите количество элементов: ";
+    std::cin >> numberOfElements;
+
+    while (numberOfElements)
+    {
+        std::cout << "[IN]: ";
+        std::cin >> elements;
+        p1.insert(elements);
+        numberOfElements--;
+    }
+
+    print(p1);
+}
